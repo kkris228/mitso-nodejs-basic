0f70f119a46d7187f2941ca8c179f5f736491963
kkris228 <butakovakristina6@gmail.com>
2023-03-15 17:18:41 +0300

    feat: delete.js completed

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import fs from "fs/promises";
+import path from "path";
+import * as url from "url";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await fs.rm(path.join(__dirname, "files", "fileToRemove.txt"))
+
+        console.log("File removed")
+    } catch (err) {
+        console.log("FS operation failed")
+    }
 };
 
 await remove();
