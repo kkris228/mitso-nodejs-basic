a55a477b7546b32d6a26c8132bef29dc2be74151
kkris228 <butakovakristina6@gmail.com>
2023-03-15 16:29:22 +0300

    feat: create.js completed

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import fs from "fs/promises";
+import path from "path";
+import * as url from "url";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const filePath = path.join(__dirname, "files", "fresh.txt");
+
+const CONTENT = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const files = await fs.readdir(path.join(__dirname, "files"));
+
+        if (files.includes("fresh.txt")) throw new Error("FS operation failed");
+
+        await fs.writeFile(filePath, CONTENT, {
+            flag: "wx"
+        });
+
+        console.log("The file has been succesfully created, the information has been entered");
+    } catch (err) {
+        console.log(err.message);
+    }
 };
 
 await create();
