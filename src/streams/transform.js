ebbda4cd9949875703c5d2b7a34059e7b949b281
kkris228 <butakovakristina6@gmail.com>
2023-03-15 23:35:23 +0300

    feat: transform.js completed

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,37 @@
+import { Transform, pipeline } from "stream";
+import { promisify } from "util";
+
+const pipelineAsync = promisify(pipeline);
+
+class MyTransform extends Transform {
+    constructor(opr) {
+        super(opr);
+    }
+
+    _transform(chunk, _, done) {
+        const transformChunk = chunk
+        .toString("utf-8")
+        .trim()
+        .split("")
+        .reverse()
+        .join("");
+
+        this.push(transformChunk + "\n");
+        done();
+    }
+}
+
 const transform = async () => {
-    // Write your code here 
+    const transformStream = new MyTransform();
+    const readableStream = process.stdin;
+    const writableStream = process.stdout;
+
+    try {
+        await pipelineAsync(readableStream, transformStream, writableStream);
+    } catch (err) {
+        process.stderr.wrire(e.message);
+        process.exit(1);
+    }
 };
 
 await transform();
