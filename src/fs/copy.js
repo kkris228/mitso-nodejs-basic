cdd871b8c414fd1aff2d468b5b335ba89f2d2e57
kkris228 <butakovakristina6@gmail.com>
2023-03-15 16:48:27 +0300

    feat: copy.js completed

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import fs from "fs/promises";
+import path from "path";
+import * as url from "url";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        await fs.cp(path.join(__dirname, "files"), path.join(__dirname, "files_copy"), {
+            recursive: true,
+            errorOnExist: true,
+            force: false
+        });
+
+        console.log("Files copied")
+    } catch (err) {
+        console.log("FS operation failed");
+    }
 };
 
 await copy();
