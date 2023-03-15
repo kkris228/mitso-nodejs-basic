f0515df3238ec925e07a74e2aefbae644c08b3bd
kkris228 <butakovakristina6@gmail.com>
2023-03-15 22:25:21 +0300

    feat:write.js completed

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,31 @@
+import { createWriteStream, WriteStream } from "fs";
+import { stdin } from "process";
+import { fileURLToPath } from "url";
+import { Writable } from "stream";
+import path from "path";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const fileToWrite = path.join(__dirname, "files", "fileToWrite.txt");
+
+class MyWritable extends Writable {
+    constructor(opr) {
+        super(opr);
+    }
+
+    input = createWriteStream(fileToWrite, {
+        encoding: "utf-8",
+    });
+
+    _write(chunk, _enc, next) {
+        this.input.write(chunk);
+        next();
+    }
+}
+
 const write = async () => {
-    // Write your code here 
+    const writableStream = new MyWritable();
+
+    stdin.pipe(writableStream);
 };
 
 await write();
